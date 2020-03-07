0d4d46bb9b661104e36757bc3e15bf7cd03eceb9
NickoEgor <egor1998nick@gmail.com>
2020-03-07 17:07:26 +0300

    Update bindings

--- config.h
+++ config.h
@@ -234,27 +234,39 @@ static Shortcut shortcuts[] = {
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
 	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_P,           selpaste,       {.i =  0} },
+
 	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
 	{ MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ MODKEY,               XK_Page_Down,   kscrolldown,    {.i = -1} },
+
 	{ MODKEY,               XK_k,           kscrollup,      {.i =  1} },
 	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
+	{ MODKEY,               XK_Up,          kscrollup,      {.i =  1} },
+	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
+
+	{ MODKEY,               XK_Home,        zoomreset,      {.f =  0} },
+	{ MODKEY | ShiftMask,   XK_R,           zoomreset,      {.f =  0} },
+
 	{ ShiftMask,            XK_Insert,      clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_S,           keyboard_select, { 0 } },
 
-    {MODKEY | ShiftMask,    XK_Up,          zoom,           {.f = +1}},
-    {MODKEY | ShiftMask,    XK_Down,        zoom,           {.f = -1}},
-    {MODKEY | ShiftMask,    XK_K,           zoom,           {.f = +1}},
-    {MODKEY | ShiftMask,    XK_J,           zoom,           {.f = -1}},
-    {MODKEY | ShiftMask,    XK_U,           zoom,           {.f = +2}},
-    {MODKEY | ShiftMask,    XK_D,           zoom,           {.f = -2}},
+    { MODKEY | ShiftMask,   XK_K,           zoom,           {.f = +1}},
+    { MODKEY | ShiftMask,   XK_J,           zoom,           {.f = -1}},
+    { MODKEY | ShiftMask,   XK_Up,          zoom,           {.f = +1}},
+    { MODKEY | ShiftMask,   XK_Down,        zoom,           {.f = -1}},
 
-    {MODKEY,                XK_o,           externalpipe,   {.v = openurlcmd}},
-    {MODKEY,                XK_y,           externalpipe,   {.v = copyurlcmd}},
+    { MODKEY | ShiftMask,   XK_U,           zoom,           {.f = +2}},
+    { MODKEY | ShiftMask,   XK_D,           zoom,           {.f = -2}},
+    { MODKEY | ShiftMask,   XK_Page_Up,     zoom,           {.f = +2}},
+    { MODKEY | ShiftMask,   XK_Page_Down,   zoom,           {.f = -2}},
+
+    { MODKEY,               XK_o,           externalpipe,   {.v = openurlcmd}},
+    { MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd}},
 };
 
 /*
